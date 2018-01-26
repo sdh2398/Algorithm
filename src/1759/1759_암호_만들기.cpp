1a487928f7073462896bc82d9e1892b97c7db81c
sdh2398 <ehdgus2398@naver.com>
2018-01-26 09:52:09 +0900

    BOJ 1759 암호 만들기

---
+++ src/1759/1759_암호_만들기.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+char pr[20];
+char ary[20];
+int l, c;
+
+int mj(char a)
+{
+	if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+		return 1;
+	return 0;
+}
+
+void pass(int cur, int pos, int mo, int ja)
+{
+	if (cur == c + 1)
+		return;
+	//printf("cur = %d pos = %d mo = %d ja = %d \n", cur, pos, mo, ja);
+
+	if (pos == l)
+	{
+		if (mo >= 1 && ja >= 2)
+			printf("%s \n", pr);
+		return;
+	}
+
+	pr[pos] = ary[cur];
+	if (mj(ary[cur]) == 1)
+		mo++;
+	else
+		ja++;
+	pass(cur + 1, pos + 1, mo, ja);
+
+	if (mj(ary[cur]) == 1)
+		mo--;
+	else
+		ja--;
+	pass(cur + 1, pos, mo, ja);
+	 
+}
+
+int main(void)
+{
+	scanf("%d %d ", &l, &c);
+
+	for (int i = 0; i < c; i++)
+		scanf("%c ", &ary[i]);
+
+	sort(ary, ary + c);
+	//chk[0] = chk[4] = chk[8] = chk[14] = chk[20] = true;
+	pass(0, 0, 0, 0);
+
+
+	return 0;
+}
